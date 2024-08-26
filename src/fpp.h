c416e91a00c29ee52023d7d3690c28eccdc577e6
Dean Krueger <dean.krueger@wisc.edu>
2024-08-26 13:13:36 -0500

    added needed resource buffers, began editing the info section at beginning of .h

--- src/fpp.h
+++ src/fpp.h
@@ -9,27 +9,28 @@ namespace tricycle {
 
 /// @class fpp
 ///
-/// This Facility is intended
-/// as a skeleton to guide the implementation of new Facility
-/// agents.
-/// The fpp class inherits from the Facility class and is
+/// The Reactor class inherits from the Facility class and is
 /// dynamically loaded by the Agent class when requested.
 ///
 /// @section intro Introduction
-/// Place an introduction to the agent here.
+/// This agnet is designed to function as a basic representation of a fusion
+/// energy system with respect to tritium flows. This is currently the alpha
+/// version of the agent, and as such some simplifying assumptions were made.
 ///
 /// @section agentparams Agent Parameters
 /// Place a description of the required input parameters which define the
-/// agent implementation.
+/// agent implementation. None so far.
 ///
 /// @section optionalparams Optional Parameters
 /// Place a description of the optional input parameters to define the
-/// agent implementation.
+/// agent implementation. None so far.
 ///
 /// @section detailed Detailed Behavior
 /// Place a description of the detailed behavior of the agent. Consider
 /// describing the behavior at the tick and tock as well as the behavior
 /// upon sending and receiving materials and messages.
+/// This has not yet been defined.
+///
 class fpp : public cyclus::Facility  {
  public:
   /// Constructor for fpp Class
@@ -57,6 +58,13 @@ class fpp : public cyclus::Facility  {
   /// The handleTick function specific to the fpp.
   /// @param time the time of the tock
   virtual void Tock();
+
+  //Resource Buffers:
+  cyclus::toolkit::ResBuf<cyclus::Material> tritium_core;
+  cyclus::toolkit::ResBuf<cyclus::Material> tritium_reserve;
+  cyclus::toolkit::ResBuf<cyclus::Material> tritium_excess;
+  cyclus::toolkit::ResBuf<cyclus::Material> helium_storage;
+  cyclus::toolkit::ResBuf<cyclus::Material> blanket;
 
   // And away we go!
 };
