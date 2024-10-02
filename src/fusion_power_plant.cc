86f39127473900252255d525fcd03ce3d578daab
Dean Krueger <dean.krueger@wisc.edu>
2024-10-02 11:03:40 -0500

    changed enternotify from pesudocode to real code and fleshed it out a bit

--- src/fusion_power_plant.cc
+++ src/fusion_power_plant.cc
@@ -12,45 +12,71 @@ std::string FusionPowerPlant::str() {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void FusionPowerPlant::EnterNotify() {
-  //Pseudocode implementation of EnterNotify()
   cyclus::Facility::EnterNotify();
-  fuel_usage_mass = (burn_rate * (fusion_power / MW_to_GW) / seconds_per_year * context()->dt());
-  fuel_usage_atoms = fuel_usage_mass / tritium_atomic_mass;
-  blanket_turnover = blanket_size * blanket_turnover_rate;
 
-  //Create the blanket material for use in the core
-  const cyclus::Composition::Ptr enriched_li = context->GetRecipe(blanket_inrecipe);
-  cyclus::Material::Ptr blanket = cyclus::Composition::CreateFromAtom(0.0, enriched_li);
+  //fuel_usage_mass = (burn_rate * (fusion_power / MW_to_GW) / 
+  //  seconds_per_year * context()->dt());
+  //fuel_usage_atoms = fuel_usage_mass / tritium_atomic_mass;
+  //blanket_turnover = blanket_size * blanket_turnover_rate;
 
-  //Truncated for readability. It goes on Tritium Storage and gets started, though.
-  fuel_startup_policy.Init(&tritium_storage).Set(fuel_incommod).Start();
-  
-  //Truncated for readability
-  blanket_fill_policy.Init(&blanket).Set(blanket_incommod).Start();
+  //Create the blanket material for use in the core, no idea if this works...
+  const Composition::Ptr enriched_li = context->GetRecipe(blanket_inrecipe);
+  Material::Ptr blanket = Composition::CreateFromAtom(0.0, enriched_li);
 
-  //Tritium Buy Policy Section:
+  fuel_startup_policy
+      .Init(this, &tritium_storage, std::string("Tritium Storage"),
+            &fuel_tracker)
+      .Set(fuel_incommod)
+      .Start();
+
+  blanket_fill_policy
+      .Init(this, &blanket_feed, std::string("Blanket Startup"), 
+            &blanket_tracker)
+      .Set(blanket_incommod)
+      .Start();
+
+  // Tritium Buy Policy Selection:
   if (refuel_mode == "schedule") {
-    IntDistribution::Ptr active_dist =
-        FixedIntDist::Ptr(new FixedIntDist(1));
-    IntDistribution::Ptr dormant_dist =
-        FixedIntDist::Ptr(new FixedIntDist(buy_frequency - 1));
-    DoubleDistribution::Ptr size_dist =
-        FixedDoubleDist::Ptr(new FixedDoubleDist(1));
-    //Do not start the policy yet.
+    IntDistribution::Ptr active_dist = 
+      FixedIntDist::Ptr(new FixedIntDist(1));
+    IntDistribution::Ptr dormant_dist = 
+      FixedIntDist::Ptr(new FixedIntDist(buy_frequency - 1));
+    DoubleDistribution::Ptr size_dist = 
+      FixedDoubleDist::Ptr(new FixedDoubleDist(1));
+
     fuel_refill_policy
-        .Init(&tritium_storage, &fuel_tracker,
-              buy_quantity, active_dist, dormant_dist, size_dis)
+        .Init(this, &tritium_storage, std::string("Input"), 
+              &fuel_tracker,
+              buy_quantity, 
+              active_dist, dormant_dist, size_dist)
         .Set(fuel_incommod);
+
   } else if (refuel_mode == "fill") {
-    //otherwise we just do it normally, but still don't start it.
-    fuel_refill_policy.Init(&tritium_storage).Set(fuel_incommod);
+    fuel_refill_policy
+        .Init(this, &tritium_storage, std::string("Input"), 
+              &fuel_tracker).Set(fuel_incommod);
+
   } else {
-    //This can be more sophisticated later.
-    throw cyclus::KeyError("Refill mode not recognized!");
+    throw KeyError("Refill mode " + refuel_mode + 
+                    " not recognized! Try 'schedule' or 'fill'.");
   }
 
-  tritium_sell_policy.Init(&tritium_excess).Set(fuel_incommod).Start();
-  helium_sell_policy.Init(&helium_storage).Set(he3).Start();
+  tritium_sell_policy
+      .Init(this, &tritium_excess, std::string("Excess Tritium"))
+      .Set(fuel_incommod)
+      .Start();
+
+  helium_sell_policy.Init(this, &helium_storage, std::string("Helium-3"))
+      .Set(he3_outcommod)
+      .Start();
+
+  //This is going to need some work... The blanket waste recipe is going to be
+  //different all the time, so we want to just peg it to "depleted lithium" or
+  //something... Come back to this when you have a second.
+  blanket_waste_sell_policy
+      .Init(this, &blanket_waste, std::string("Blanket Waste"))
+      .Set(blanket_outcommod)
+      .Start();
 
 }
 
