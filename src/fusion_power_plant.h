fc90dc336730a778ddf78c8675fe81e96f47924e
Dean Krueger <dean.krueger@wisc.edu>
2024-10-03 13:12:10 -0500

    Added necessary code to header file to support EnterNotify()

--- src/fusion_power_plant.h
+++ src/fusion_power_plant.h
@@ -18,11 +18,11 @@ namespace tricycle {
 ///
 /// @section agentparams Agent Parameters
 /// Place a description of the required input parameters which define the
-/// agent implementation. None so far.
+/// agent implementation. Saving for Later.
 ///
 /// @section optionalparams Optional Parameters
 /// Place a description of the optional input parameters to define the
-/// agent implementation. None so far.
+/// agent implementation. Saving for later.
 ///
 /// @section detailed Detailed Behavior
 /// Place a description of the detailed behavior of the agent. Consider
@@ -47,9 +47,6 @@ class FusionPowerPlant : public cyclus::Facility  {
 
   #pragma cyclus note {"doc": "A stub facility is provided as a skeleton " \
                               "for the design of new facility agents."}
-  //Functions:
-  /// A verbose printer for the FusionPowerPlant
-  virtual std::string str();
 
   /// The handleTick function specific to the FusionPowerPlant.
   /// @param time the time of the tick
@@ -59,27 +56,103 @@ class FusionPowerPlant : public cyclus::Facility  {
   /// @param time the time of the tock
   virtual void Tock();
 
-  //Member Variables:
+  /// A verbose printer for the FusionPowerPlant
+  virtual std::string str();
 
-  //Functions:
-  void CycleBlanket();
+  //State Variables:
+  #pragma cyclus var { \
+    "doc": "Fresh fuel commodity", \
+    "tooltip": "Name of fuel commodity requested", \
+    "uilabel": "Fuel input commodity" \
+  }
+  std::string fuel_incommod;
 
-  //Resource Buffers:
+  #pragma cyclus var { \
+    "default": 'fill', \
+    "doc": "Method of refueling the reactor", \
+    "tooltip": "Options: 'schedule' or 'fill'", \
+    "uitype": "combobox", \
+    "categorical": ['schedule', 'fill'], \
+    "uilabel": "Refuel Mode" \
+  }
+  std::string refuel_mode;
+
+  #pragma cyclus var { \
+    "default": 0.1, \
+    "doc": "Quantity of fuel reactor tries to purchase in schedule mode", \
+    "tooltip": "Defaults to 100g/purchase", \
+    "units": "kg", \
+    "uitype": "range", \
+    "range": [0, 1e299], \
+    "uilabel": "Buy quantity" \
+  }
+  double buy_quantity;
+
+  #pragma cyclus var { \
+    "doc": "Helium-3 output commodity Designation", \
+    "tooltip": "He-3 output commodity", \
+    "uilabel": "He-3 output commodity" \
+  }
+  std::string he3_outcommod;
+
+  #pragma cyclus var { \
+    "doc": "Recipe for blanket feed material", \
+    "tooltip": "Recipe for blanket feed material", \
+    "uilabel": "Recipe for blanket feed material" \
+  }
+  std::string blanket_inrecipe;
+
+  #pragma cyclus var { \
+    "doc": "Blanket waste commodity designation", \
+    "tooltip": "Blanket waste commodity", \
+    "uilabel": "Blanket waste commodity" \
+  }
+  std::string blanket_outcommod;
+
+  //Resource Buffers and Trackers:
   cyclus::toolkit::ResBuf<cyclus::Material> tritium_storage;
   cyclus::toolkit::ResBuf<cyclus::Material> tritium_excess;
   cyclus::toolkit::ResBuf<cyclus::Material> helium_excess;
   cyclus::toolkit::ResBuf<cyclus::Material> blanket_feed;
   cyclus::toolkit::ResBuf<cyclus::Material> blanket_waste;
 
-  //PyneIDs
-  const int tritium_id = 10030000;
+  cyclus::toolkit::MatlBuyPolicy fuel_startup_policy;
+  cyclus::toolkit::MatlBuyPolicy fuel_refill_policy;
+  cyclus::toolkit::MatlBuyPolicy blanket_fill_policy;
 
-  //Compositions:
-  const cyclus::CompMap T = {{tritium_id, 1}};
-  const cyclus::Composition::Ptr tritium_comp = cyclus::Composition::CreateFromAtom(T);
+  cyclus::toolkit::MatlSellPolicy tritium_sell_policy;
+  cyclus::toolkit::MatlSellPolicy helium_sell_policy;
+  cyclus::toolkit::MatlSellPolicy blanket_waste_sell_policy;
 
-  //Materials:
-  cyclus::Material::Ptr sequestered_tritium = cyclus::Material::CreateUntracked(0.0, tritium_comp);
+  cyclus::toolkit::TotalInvTracker fuel_tracker;
+  cyclus::toolkit::TotalInvTracker blanket_tracker;
+
+  //Functions:
+  void CycleBlanket();
+  bool CheckOpeartingConditions();
+  void SequesterTritium();
+  void OperateReactor();
+  void CycleBlanket();
+  void DecayInventories();
+  void ExtractHelium();
+  void MoveExcessTritiumToSellBuffer();
+
+  private:
+    //This is to correctly instantiate the TotalInvTracker(s)
+    double fuel_limit = 1000.0;
+    double blanket_limit = 100000.0; 
+    Material:Ptr blanket;
+
+
+    //NucIDs for Pyne
+    const int tritium_id = 10030000;
+
+    //Compositions:
+    const cyclus::CompMap T = {{tritium_id, 1}};
+    const cyclus::Composition::Ptr tritium_comp = cyclus::Composition::CreateFromAtom(T);
+
+    //Materials:
+    cyclus::Material::Ptr sequestered_tritium = cyclus::Material::CreateUntracked(0.0, tritium_comp);
 
   // And away we go!
 };
