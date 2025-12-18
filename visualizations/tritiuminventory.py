d2f2efa265ac42e974adc0e0416eb196c2d0bc8e
trysarahtops <sgfrancis@wisc.edu>
2025-12-18 17:51:27 -0600

    made changes to variables and axes

--- visualizations/tritiuminventory.py
+++ visualizations/tritiuminventory.py
@@ -16,15 +16,27 @@ info = """SELECT "_rowid_",* FROM "main"."Info" LIMIT 1 OFFSET 0;"""
 tritium = pandas.read_sql(sql,conn)
 date = pandas.read_sql(info,conn)
 
-year = date.InitialYear[0]
+#start year definition
+year1 = date.InitialYear[0]
 
-plt.plot(year + tritium.Time/12, tritium.Quantity)
+#converting timesteps from months to years
+time = tritium.Time/12
 
-#Add labels, axes, and title
+#plotting tritium inventory over time
+plt.plot(year1 + time, tritium.Quantity)
+
+#selects maximum amount of tritium plus 5%
+apex = 1.05 * max(tritium.Quantity)
+
+#selects maximum time plus 5% for time axis
+year2 = year1 + 1.05 * max(time)
+
+#Add labels and title
 plt.xlabel("Year")
-plt.xlim(year,year+75)
+
+plt.xlim(year1, year2)
 plt.ylabel("Tritium [kg]")
-plt.ylim(0,50)
+plt.ylim(0, apex)
 plt.title("Tritium Inventory")
 
 #Show the plot
