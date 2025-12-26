34b11a565ee54d96ab72104857f1bd15332a6114
trysarahtops <160659228+trysarahtops@users.noreply.github.com>
2025-12-26 17:03:31 -0600

    Update FPPscript.py
    Added comments to code

--- deployment_scenarios/FPPscript.py
+++ deployment_scenarios/FPPscript.py
@@ -1,22 +1,26 @@
 import xml.etree.cElementTree as ET
 import csv
 
+# function to create list from file
 def csv_to_list(file):
-    FPP = []
 
     with open(file, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
-
         data_list = list(reader)
     return data_list
 
+# creating lists from file
 csv_data = csv_to_list('FPP_input.csv')[1]
 FPP_data = csv_to_list('FPP_input.csv')[3]
+
+# printing lists to check correct selection, can be commented out
 print(csv_data)
 print(FPP_data)
 
 tree = ET.parse('SampleFPP.xml')
 
+
+# substituting values into FPP definition
 tree.find("./facility/name").text = csv_data[0]
 tree.find(".//fusion_power").text = csv_data[1]
 tree.find(".//TBR").text = csv_data[2]
@@ -35,6 +39,7 @@ tree.find(".//blanket_size").text = csv_data[14]
 tree.find(".//blanket_turnover_fraction").text = csv_data[15]
 tree.find(".//blanket_turnover_frequency").text = csv_data[16]
 
+# substituting values into region
 tree.find("./region/name").text = FPP_data[0]
 tree.find("./region/institution/name").text = FPP_data[1]
 tree.find(".//prototypes/val").text = csv_data[0]
@@ -42,4 +47,5 @@ tree.find(".//build_times/val").text = FPP_data[2]
 tree.find(".//lifetimes/val").text = FPP_data[3]
 tree.find(".//n_build/val").text = FPP_data[4]
 
-tree.write("output.xml")
+# output file
+tree.write("FPPoutput.xml")
